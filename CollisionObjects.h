f75c6d7736178163efe28ee50e2b2e19820456a7
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2019-11-17 14:56:05 -0800

    Update CollisionObjects.h

--- CollisionObjects.h
+++ CollisionObjects.h
@@ -1,29 +1,18 @@
-#ifndef COOLGAME_COLLISIONOBJECTS_H
-#define COOLGAME_COLLISIONOBJECTS_H
+#ifndef COOLGAME_COLLISION_OBJECTS_H
+#define COOLGAME_COLLISION_OBJECTS_H
 
 #include <SFML/Graphics.hpp>
-#include "character.h"
 
 using namespace sf;
 
 
-class CollisionObject {
-protected:
+class Wall {
+public:
     Vector2f position;
     float width, height;
-
-public:
-    CollisionObject(float x, float y, float w, float h, const Texture& texture);
-    CollisionObject(float x, float y, float w, float h, Color color);
-    bool checkCollision(Character character);
-    Character checkCollision(Character character[]);
-    RectangleShape shape;
-};
-
-class Wall : public CollisionObject {
-public:
     Wall(float x, float y, float w, float h, const Texture& texture);
     Wall(float x, float y, float w, float h, Color color);
+    RectangleShape shape;
 };
 
 
