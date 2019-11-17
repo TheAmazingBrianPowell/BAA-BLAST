ac6194a92fc26f42e9dea90fa7f378c4c6f28d5d
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2019-11-17 14:55:43 -0800

    Update CollisionObjects.cpp

--- CollisionObjects.cpp
+++ CollisionObjects.cpp
@@ -1,5 +1,6 @@
 #include "CollisionObjects.h"
-CollisionObject::CollisionObject(float x, float y, float w, float h, const Texture& texture)
+
+Wall::Wall(float x, float y, float w, float h, const Texture& texture)
 {
     position = Vector2f(x,y);
     width = w;
@@ -9,7 +10,7 @@ CollisionObject::CollisionObject(float x, float y, float w, float h, const Textu
     shape.setTexture(&texture);
 }
 
-CollisionObject::CollisionObject(float x, float y, float w, float h, Color color)
+Wall::Wall(float x, float y, float w, float h, Color color)
 {
     width = w;
     height = h;
@@ -18,35 +19,3 @@ CollisionObject::CollisionObject(float x, float y, float w, float h, Color color
     shape.setSize(Vector2f(width,height));
     shape.setFillColor(color);
 }
-
-bool CollisionObject::checkCollision(Character character)
-{
-    return character.position.x + character.width >= position.x && character.position.x <= position.x + width &&
-    character.position.y + character.height >= position.y && character.position.y <= position.y + height;
-}
-
-Character CollisionObject::checkCollision(Character character[]) {
-    //Character characters[2];
-//    for (var i = 0; i < character.length; i++) {}
-//    if (character[].position.x + character[].width >= position.x && character[].position.x <= position.x + width &&
-//        character[].position.y + character[].height >= position.y && character[].position.y <= position.y + height) {
-//        return character[i]
-//    }
-//    }
-}
-
-
-
-
-//Wall inherits from CollisionObject
-Wall::Wall(float x, float y, float w, float h, const Texture& texture) : CollisionObject(x,y, w, h, texture)
-{
-
-}
-
-Wall::Wall(float x, float y, float w, float h, Color color) : CollisionObject(x,y, w, h, color)
-{
-
-}
-
-
