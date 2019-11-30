d3d35da15d9a1cf8d9880c5370ab60890394232a
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2019-11-30 00:07:08 -0800

    Update character.h

--- character.h
+++ character.h
@@ -7,33 +7,57 @@
 
 using namespace sf;
 
+class Enemy {
+private:
+    short direction = 1;
+    bool isOnBlock;
+
+public:
+    float width, height;
+
+    Sprite shape;
+
+    Vector2f position;
+
+    Enemy(float x, float y, float w, float h, const Texture& texture);
+
+    void move();
+
+    void collide(Wall wall);
+
+    void update();
+};
+
 class Character {
 private:
     float ACCELERATION, VELOCITY_LIMIT;
-    void constrain(float &value, float min, float max);
+    static void constrain(float &value, float min, float max);
 
 public:
     Character(float x, float y, float w, float h, Texture& texture);
 
-    Character(float x, float y, float w, float h, Color color);
-
     Vector2f velocity, position;
+    bool isAlive;
+    bool canJump;
 
-    bool canJump, isAlive;
+    int lives;
 
-    RectangleShape shape;
+    Sprite shape;
 
     float width, height, jumpTime;
 
-    void checkGroundCollision();
 
     void jump();
 
     void controls();
 
-    void applyForce(Vector2f force);
-
     void collide(Wall wall);
+
+    void collide(Enemy enemy);
+
+    void collide(Portal portal, bool& level);
+
+    bool collide(Sign sign);
 
     void update(int loopTimes);
 };
