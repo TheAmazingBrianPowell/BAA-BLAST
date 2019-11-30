181694d7cad36e4574f8872ce5bcd83d03166339
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2019-11-30 00:07:37 -0800

    Update CollisionObjects.h

--- CollisionObjects.h
+++ CollisionObjects.h
@@ -5,15 +5,35 @@
 
 using namespace sf;
 
-
-class Wall {
+class CollisionObject {
 public:
     Vector2f position;
     float width, height;
-    Wall(float x, float y, float w, float h, const Texture& texture);
-    Wall(float x, float y, float w, float h, Color color);
-    RectangleShape shape;
+    CollisionObject(float x, float y, float w, float h, const Texture& texture);
+    CollisionObject(float x, float y, float w, float h, Color color);
+    CollisionObject(float x, float y, float w, float h);
+    Sprite shape;
 };
 
+class Wall : public CollisionObject {
+public:
+    Wall(float x, float y, float w, float h, const Texture& texture);
+    Wall(float x, float y, float w, float h, Color color);
+    Wall(float x, float y, float w, float h);
+};
+
+class Portal : public CollisionObject{
+public:
+    Portal(float x, float y, float w, float h, const Texture& texture);
+    Portal(float x, float y, float w, float h, Color color);
+    Portal(float x, float y, float w, float h);
+};
+
+class Sign : public CollisionObject {
+public:
+    Sign(float x, float y, float w, float h, const Texture& texture);
+    Sign(float x, float y, float w, float h, Color color);
+    Sign(float x, float y, float w, float h);
+};
 
 #endif
