9dad89e2dc447bae7a7014a569aacbe1135bf587
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2019-11-30 00:05:07 -0800

    Update CollisionObjects.cpp

--- CollisionObjects.cpp
+++ CollisionObjects.cpp
@@ -1,21 +1,38 @@
 #include "CollisionObjects.h"
 
-Wall::Wall(float x, float y, float w, float h, const Texture& texture)
+CollisionObject::CollisionObject(float x, float y, float w, float h, const Texture& texture)
 {
     position = Vector2f(x,y);
     width = w;
     height = h;
     shape.setPosition(position);
-    shape.setSize(Vector2f(width,height));
-    shape.setTexture(&texture);
+    shape.setTexture(texture);
 }
 
-Wall::Wall(float x, float y, float w, float h, Color color)
+CollisionObject::CollisionObject(float x, float y, float w, float h, Color color)
 {
     width = w;
     height = h;
     position = Vector2f(x,y);
     shape.setPosition(position);
-    shape.setSize(Vector2f(width,height));
-    shape.setFillColor(color);
+    shape.setColor(color);
 }
+
+CollisionObject::CollisionObject(float x, float y, float w, float h)
+{
+    width = w;
+    height = h;
+    position = Vector2f(x,y);
+}
+
+Wall::Wall(float x, float y, float w, float h, const Texture& texture) : CollisionObject(x, y, w, h, texture) {}
+Wall::Wall(float x, float y, float w, float h, Color color) : CollisionObject(x, y, w, h, color) {}
+Wall::Wall(float x, float y, float w, float h) : CollisionObject(x, y, w, h) {}
+
+Portal::Portal(float x, float y, float w, float h, const Texture& texture) : CollisionObject(x, y, w, h, texture) {}
+Portal::Portal(float x, float y, float w, float h, Color color) : CollisionObject(x, y, w, h, color) {}
+Portal::Portal(float x, float y, float w, float h) : CollisionObject(x, y, w, h) {}
+
+Sign::Sign(float x, float y, float w, float h, const Texture& texture) : CollisionObject(x, y, w, h, texture) {}
+Sign::Sign(float x, float y, float w, float h, Color color) : CollisionObject(x, y, w, h, color) {}
+Sign::Sign(float x, float y, float w, float h) : CollisionObject(x, y, w, h) {}
