6dbfb007b929a84d8d1a649f008bd5993f374e04
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2019-12-23 17:49:33 -0800

    Update CollisionObjects.cpp

--- CollisionObjects.cpp
+++ CollisionObjects.cpp
@@ -22,3 +22,32 @@ Wall::Wall(float x, float y, float w, float h) : CollisionObject(x, y, w, h) {}
 Portal::Portal(float x, float y, float w, float h, const Texture& texture) : CollisionObject(x, y, w, h, texture) {}
 
 Sign::Sign(float x, float y, float w, float h, const Texture& texture) : CollisionObject(x, y, w, h, texture) {}
+
+Coin::Coin(float x, float y, float w, float h, const Texture& texture) : CollisionObject(x, y, w, h, texture) {
+    rotation = 1;
+    rotateDirection = true;
+}
+
+void Coin::rotate() {
+    if(rotateDirection) {
+    	rotation -= 0.05;
+	shape.setPosition(shape.getPosition().x + 0.5, shape.getPosition().y);
+    } else {
+	rotation += 0.05;
+	shape.setPosition(shape.getPosition().x - 0.5, shape.getPosition().y);
+    }
+    if(rotation <= -1) {
+    	rotateDirection = false;
+    }
+    if(rotation >= 1) {
+    	rotateDirection = true;
+    }
+    shape.setScale(rotation, 1);
+}
+
+Jump::Jump(float x, float y, float w, float h, const Texture& texture, const Texture& texture2) : CollisionObject(x,y,w,h,texture) {
+    firstTexture = &texture;
+    secondTexture = &texture2;
+    originalPos = Vector2f(x,y);
+    springTimer = 0;
+}
